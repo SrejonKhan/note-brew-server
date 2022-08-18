a2af5d70adea53f765cf018d665806190ea4a43c
Srejon Khan <srejonkhan79@gmail.com>
2022-08-18 18:22:23 +0600

    add docs for user/me endpoint

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,13 +1,14 @@
 from flask import Blueprint
 from flask_jwt_extended import jwt_required, current_user
+from flasgger import swag_from
 
 user_api_bp = Blueprint("user_api", __name__)
 
-
 @user_api_bp.route('/me')
 @jwt_required()
+@swag_from("../../docs/user/me.yml")
 def me():
     user = current_user
-    return user.user_id
+    return user.jsonify_self(), 200
 
 
