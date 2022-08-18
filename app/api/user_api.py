b3e1ec4a21d09fa0ed662691eaab32c816b85c3d
Srejon Khan <srejonkhan79@gmail.com>
2022-08-18 18:22:58 +0600

    add url prefix for user api endpoints

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask_jwt_extended import jwt_required, current_user
 from flasgger import swag_from
 
-user_api_bp = Blueprint("user_api", __name__)
+user_api_bp = Blueprint("user_api", __name__, url_prefix="/user")
 
 @user_api_bp.route('/me')
 @jwt_required()
