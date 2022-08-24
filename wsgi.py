e4c0f2cbd139cbd93eac8be469b47c815c1af1cb
Srejon Khan <srejonkhan79@gmail.com>
2022-08-25 02:24:47 +0600

    update config profile loading

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
+import os
 from app import create_app
-from app.configs.config import Config, DevelopmentConfig, TestingConfig, ProductionConfig
 
-app = create_app(DevelopmentConfig)
+CONFIG_TYPE = os.getenv('CONFIG_TYPE', default='app.configs.config.DevelopmentConfig')
+app = create_app(CONFIG_TYPE)
 
 if __name__ == "__main__":
     app.run()
