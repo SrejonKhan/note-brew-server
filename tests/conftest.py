daa5ca02a6909e0130200c7cfea465582ef88ed3
Srejon Khan <srejonkhan79@gmail.com>
2022-08-24 20:59:13 +0600

    add github action for ci

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from app.models import User
 from app import create_app 
+from app.configs.config import TestingConfig
 
 @pytest.fixture(scope="module")
 def new_user():
@@ -9,7 +10,7 @@ def new_user():
 
 @pytest.fixture(scope="module")
 def test_client():
-    app = create_app()
+    app = create_app(TestingConfig)
     
     with app.test_client() as testing_client:
         yield testing_client
